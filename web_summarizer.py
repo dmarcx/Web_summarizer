87a6a6b6365763b72c5a9ae8d6c23d2b7ce1ed6f
dmarcx <72644283+dmarcx@users.noreply.github.com>
2025-06-11 22:01:45 +0300

    Update web_summarizer.py

--- web_summarizer.py
+++ web_summarizer.py
@@ -212,4 +212,6 @@ with gr.Blocks(css=rtl_css) as demo:
         outputs=[output_file, output_text]
     )
 
-demo.launch(inbrowser=True)
+import os
+port = int(os.environ.get("PORT", 7860))
+demo.launch(server_name="0.0.0.0", server_port=port)
